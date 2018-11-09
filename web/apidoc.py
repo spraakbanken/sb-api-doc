03ad591a72409a937706d9621e18c778622d7a7d
Martin Hammarstedt <martin.hammarstedt@gu.se>
2018-11-09 17:29:31 +0100

    Replace static HTML generation with Flask app

---
+++ web/apidoc.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, abort
+import os
+import yaml
+import urllib.request
+import re
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    result = []
+    for api in apis:
+        result.append("""<li><img src="{favicon}"> <a href="{path}">{name}</a> &ndash; {description}</li>""".format(**apis[api]))
+    return index_template.replace("{{api-list}}", "".join(result))
+
+
+@app.route("/<path>")
+def show_api(path):
+    if not path in apis:
+        abort(404)
+    api = apis[path]
+    html = api_template.replace("{{title}}", api["name"] + " API").replace("{{spec-url}}", api["oas-file"]).replace(
+        "{{topbar}}", "none").replace("{{favicon}}", api.get("favicon", ""))
+    return html
+
+
+@app.route("/test")
+def oas_test():
+    html = api_template.replace("{{title}}", "OAS Test").replace("{{spec-url}}", "").replace("{{topbar}}", "show")
+    return html
+
+
+@app.route("/reload")
+def read_register():
+    global apis
+    apis = {}
+
+    if re.match(r"https?:\/\/.*", config["register"]):
+        with urllib.request.urlopen(config["register"]) as response:
+            register_data = response.read()
+    else:
+        with open(config["register"]) as infile:
+            register_data = infile.read()
+
+    for api in yaml.load(register_data)["apis"]:
+        apis[api["path"]] = api
+
+    return "API register updated"
+
+
+# Read config
+if os.path.isfile("config.yaml"):
+    config_file = "config.yaml"
+else:
+    print("Using default config. Create config.yaml to customize.")
+    config_file = "config_default.yaml"
+
+with open(config_file) as infile:
+    config = yaml.load(infile)
+
+# Load templates
+with open(config["index-template"]) as infile:
+    index_template = infile.read()
+
+with open(config["api-template"]) as infile:
+    api_template = infile.read()
+
+read_register()
+
+if __name__ == "__main__":
+    # Run using Flask (use only for development)
+    app.run(debug=True, threaded=True, host="0.0.0.0")
